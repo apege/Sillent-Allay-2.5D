ef21d7be6ea2f923c326c5dbcb296ccb33e43feb
unknown <alifmuzakki1110@gmail.com>
2026-05-17 16:14:28 +0700

    fix bug

--- Assets/Scripts/Story/TimelineCutsceneManager.cs
+++ Assets/Scripts/Story/TimelineCutsceneManager.cs
@@ -9,18 +9,18 @@ public class TimelineCutsceneManager : MonoBehaviour
     [Header("Scene setelah cutscene selesai")]
     [SerializeField] private string nextSceneName;
 
+    [Header("Spawn Point")]
+    [SerializeField] private string targetSpawnPointID; // isi ID spawn point tujuan
+
     private void Start()
     {
         if (playableDirector == null)
         {
-            Debug.LogError("[CutsceneManager] PlayableDirector tidak ditemukan!");
+            Debug.LogError("[TimelineCutsceneManager] PlayableDirector tidak ditemukan!");
             return;
         }
 
-        // Subscribe ke event selesai
         playableDirector.stopped += OnCutsceneFinished;
-
-        // Play cutscene
         playableDirector.Play();
     }
 
@@ -32,9 +32,13 @@ public class TimelineCutsceneManager : MonoBehaviour
 
     private void OnCutsceneFinished(PlayableDirector director)
     {
+        // Set spawn point sebelum pindah scene
+        if (!string.IsNullOrEmpty(targetSpawnPointID))
+            SpawnPointManager.nextSpawnPointID = targetSpawnPointID;
+
         if (SceneTransitionManager.Instance != null)
             SceneTransitionManager.Instance.TransitionToScene(nextSceneName);
         else
-            Debug.LogError("[CutsceneManager] SceneTransitionManager tidak ditemukan!");
+            Debug.LogError("[TimelineCutsceneManager] SceneTransitionManager tidak ditemukan!");
     }
 }
